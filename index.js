4422e427d8882560b3825f328e2195bda14bb551
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2023-01-11 16:19:55 +0700

    index

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const product = require("./routes/product");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-const product = require("./api/product");
+app.use(cors());
+
 
 app.use(express.json({ extended: false }));
-
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("Connect database successful");
+});
+app.use(bodyParser.json({limit:"50mb"}));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
