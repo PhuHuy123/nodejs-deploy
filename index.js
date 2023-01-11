fbc82c63cabdf459bb6a2829d8f6cf1d581af1ad
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2023-01-11 16:09:08 +0700

    fix

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
